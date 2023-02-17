49b47eea91f0e8001ad00a64c18f39debe970064
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-17 18:12:33 +0000

    Lab2Day1

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Application myObject = new Application();
+        return myObject;
     }
 }
